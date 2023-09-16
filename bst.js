ee9912faeb1841a1ad8d366162b220917c2e1aca
confett0 <118047961+confett0@users.noreply.github.com>
2023-09-16 17:35:33 +0200

    Create Node and Tree classes

---
+++ bst.js
@@ -0,0 +1,17 @@
+class Node {
+constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
+}
+
+class Tree {
+    constructor(arr) {
+        this.root = root;
+    }
+}
+
+const buildTree = (array) => {
+    return root;
+}
