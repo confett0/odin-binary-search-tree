ac5359f8925e51aeef8666c934e472b6292eb0ca
confett0 <118047961+confett0@users.noreply.github.com>
2023-09-16 18:01:29 +0200

    Create buildTree and add prettyPrint method

--- bst.js
+++ bst.js
@@ -1,17 +1,44 @@
 class Node {
-constructor(value) {
-    this.value = value;
+  constructor(data) {
+    this.data = data;
     this.left = null;
     this.right = null;
-}
+  }
 }
 
 class Tree {
-    constructor(arr) {
-        this.root = root;
+  constructor(array) {
+    const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+    this.root = this.buildTree(sortedArray);
+  }
+
+  buildTree(arr, start = 0, end = arr.length - 1) {
+    if (start > end) {
+      return null;
     }
+
+    const middle = Math.floor((start + end) / 2);
+    const node = new Node(arr[middle]);
+
+    node.left = this.buildTree(arr, start, middle - 1);
+    node.right = this.buildTree(arr, middle + 1, end);
+
+    return node;
+  }
+
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
 }
 
-const buildTree = (array) => {
-    return root;
-}
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+tree.prettyPrint(tree.root);
